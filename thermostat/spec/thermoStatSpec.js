2adc21c95bb95645d58cd74d7f7efdac45d10083
James Turner <mail.jt@gmail.com>
2017-02-01 12:33:12 +0000

    weee

--- thermostat/spec/thermoStatSpec.js
+++ thermostat/spec/thermoStatSpec.js
@@ -3,40 +3,71 @@ var thermostat;
 var temperature;
 
 describe('thermostat',function(){
-
+  var defaultTemp
 
   beforeEach(function(){
     thermostat = new ThermoStat;
+    defaultTemp = thermostat._START_TEMP;
   });
 
-
   describe('start with default temperature', function(){
-    it('start at 20 degree',function(){
-      expect(thermostat.temperature).toEqual(20);
+    it('starts at default',function(){
+      expect(thermostat.temperature).toEqual(defaultTemp);
+    });
+  });
+
+  describe('#isPowerSaving',function(){
+    it('starts with power saving on', function(){
+      expect(thermostat.isPowerSaving).toEqual(true);
     });
   });
 
   describe('#up', function(){
     it('increases temperature', function(){
-      thermostat.temperature = 20;
+      thermostat.temperature = defaultTemp;
       thermostat.up();
-      expect(thermostat.temperature).toEqual(21);
+      expect(thermostat.temperature).toEqual(defaultTemp + 1);
+    });
+    describe('with power save on',function(){
+      it('won\'t go above max temp',function (){
+        var defaultMax = thermostat._MAX_TEMP_SAVING;
+        thermostat.temperature = defaultMax;
+        thermostat.up();
+        expect(thermostat.temperature).toEqual(defaultMax);
+      });
+    });
+    describe('with power save off',function(){
+      it('won\'t go above max temp',function (){
+        var defaultMax = thermostat._MAX_TEMP_NO_SAVE;
+        thermostat.temperature = defaultMax;
+        thermostat.up();
+        expect(thermostat.temperature).toEqual(defaultMax);
+      });
     });
   });
 
   describe('#down', function(){
     it('decreases temperature', function(){
-      thermostat.temperature = 20;
+      thermostat.temperature = defaultTemp;
       thermostat.down();
-      expect(thermostat.temperature).toEqual(19);
+      expect(thermostat.temperature).toEqual(defaultTemp - 1);
     });
 
     it('has a minimum temperature', function(){
-      thermotstat.temperature.minimum();
-      expect(thermotstat.temperature).toEqual(10);
+      var defaultMin = thermostat._MIN_TEMP;
+      thermostat.temperature = defaultMin;
+      thermostat.down();
+      expect(thermostat.temperature).toEqual(defaultMin);
     });
   });
 
-
-
+  describe('#_max_temp', function(){
+    it ('returns MAX_TEMP_SAVING if power saving is on', function(){
+      expect(thermostat._maxTemp()).toEqual(thermostat._MAX_TEMP_SAVING);
+    });
+    it ('returns MAX_TEMP_NO_SAVE if power saving is off', function(){
+      thermostat.isPowerSaving = false;
+      expect(thermostat._maxTemp()).toEqual(thermostat._MAX_TEMP_NO_SAVE);
+    });
+  });
 });
--- thermostat/src/thermoStat.js
+++ thermostat/src/thermoStat.js
@@ -1,13 +1,24 @@
 'use strict';
 function ThermoStat(){
   this._START_TEMP = 20;
+  this._MIN_TEMP = 10;
+  this._MAX_TEMP_NO_SAVE = 32;
+  this._MAX_TEMP_SAVING = 25;
   this.temperature = this._START_TEMP;
+  this.isPowerSaving = true;
 }
 
 ThermoStat.prototype.up = function (){
+  if(this.temperature + 1 > this._maxTemp()){return}
   this.temperature += 1;
 }
 
 ThermoStat.prototype.down = function(){
+  if(this.temperature -1 < this._MIN_TEMP){return}
   this.temperature -= 1;
+}
+
+ThermoStat.prototype._maxTemp = function(){
+  if(this.isPowerSaving){return this._MAX_TEMP_SAVING}
+  return this._MAX_TEMP_NO_SAVE
 }
