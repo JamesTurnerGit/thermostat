ecd708361656bbdb2778e87e49c758bd552be411
James Turner <mail.jt@gmail.com>
2017-02-01 16:21:47 +0000

    started to add weather api

--- thermostat/index.js
+++ thermostat/index.js
@@ -2,6 +2,7 @@ var thermoStat= new ThermoStat()
 
 $(document).ready(function() {
   refreshDisplay()
+  getWeather()
   $('#thermoStat_temp_up').click(function(){
     thermoStat.up()
     refreshDisplay()
@@ -26,10 +27,21 @@ $(document).ready(function() {
 
 })
 
+function getWeather(){
+  url = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=3f6c140e6471330359fda6d41c526a07"
+  jQuery.get(url,function(data){
+   console.log(data.main.temp)
+  })
+}
+
+
 function refreshDisplay(){
   $('#temp_display').text("current temp: " + thermoStat.temperature +" degrees")
   $('#power_save_display').text("power save on: " + thermoStat.isPowerSaving)
   $('#energy_usage_display').text("energy usage: " + thermoStat.usage())
-  if thermoStat.usage === 'low-usage'
-  $('#energy_usage_display').css('background-color',green )
+  var color
+  if(thermoStat.usage() === 'low-usage'){color = "green"}
+  if(thermoStat.usage() === 'medium-usage'){color = "black"}
+  if(thermoStat.usage() === 'high-usage'){color = "red"}
+  $('#energy_usage_display').css('color',color)
 }
